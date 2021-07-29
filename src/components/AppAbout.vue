a5199dbf429083e27112606a1ccc926d5d334d72
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2021-07-29 10:57:32 +0800

    fix: Grammar on About Section

--- src/components/AppAbout.vue
+++ src/components/AppAbout.vue
@@ -11,11 +11,12 @@
     <div class="text-justify text-gray-700 font-light text-lg col-span-3">
       <p class="font-bold text-xl mb-4">Who am I?</p>
 
-      <p class="mb-4">I am Lenard Mangay-ayam, A Web Developer.</p>
+      <!-- Thanks to [wloisv@gmail.com] for correcting my mistakes -->
+      <p class="mb-4">Hey there! I'm Lenard Mangay-ayam, a web developer based in the historic city of Caloocan.</p>
 
-      <p class="mb-4">I'm Currently Residing at Caloocan City in the Philippines. 19 Years old and currently a 3rd Year Computer Science student at University of Caloocan City. I have been coding for 5 years and have learned a lot of things a long the way.</p>
+      <p class="mb-4">I'm 19 Years old and currently a 3rd year Computer Science student at University of Caloocan City. I have been coding for 5 years and have learned a lot of things along the way.</p>
 
-      <p class="mb-4">I am inlove with Web Development because it is a way for me to express my talent and skills and to create an application that many people can benefit.</p>
+      <p class="mb-4">I am in love with web development as it is a way for me to express myself, my talents, and my skills by creating applications that many people can benefit from.</p>
 
       <a href="#" class="bg-purple-700 text-white text-sm inline-block py-2 px-4 rounded-lg border-purple-900 hover:bg-purple-900">Download CV</a>
     </div>
